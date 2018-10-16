e366ea02e50a066f4da73556c8dc4d326c931fc6
John Clary <john.clary@austintexas.gov>
2018-10-16 15:51:32 -0500

    Actually point to the API

--- js/map.js
+++ js/map.js
@@ -2,7 +2,7 @@
 // deploy api
 // highlight source cells
 
-var API_URL = 'https://dockless-data.austintexas.io'
+var API_URL = 'https://dockless-data.austintexas.io/api'
 
 var formatPct = d3.format(".1%");
 
