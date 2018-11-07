4cd38888af642c4b98eefdad3b91239b86fb2dc6
Mateo Clarke <code@mateoclarke.com>
2018-11-07 11:47:32 -0600

    add HtmlCriticalWebpackPlugin

--- webpack.prod.js
+++ webpack.prod.js
@@ -1,6 +1,22 @@
 const merge = require("webpack-merge");
 const common = require("./webpack.common.js");
+const HtmlCriticalWebpackPlugin = require("html-critical-webpack-plugin");
 
 module.exports = merge(common, {
-  mode: "production"
+  mode: "production",
+  plugins: [
+    new HtmlCriticalWebpackPlugin({
+      base: path.resolve(__dirname, "dist"),
+      src: "index.html",
+      dest: "index.html",
+      inline: true,
+      minify: true,
+      extract: true,
+      width: 375,
+      height: 565,
+      penthouse: {
+        blockJSRequests: false
+      }
+    })
+  ]
 });
