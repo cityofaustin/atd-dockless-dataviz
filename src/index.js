cc7e1d5cc67beb93089ad3fb0615d8cbb52e648d
Mateo Clarke <code@mateoclarke.com>
2018-11-29 17:02:25 -0600

    don’t overwrite base 100vh for map-container on dekstop

--- src/index.js
+++ src/index.js
@@ -11,6 +11,7 @@ import { ckmeans } from "simple-statistics";
 // We have to import js-cookies this way because they need better support for
 // es6 module import: https://github.com/js-cookie/js-cookie/issues/233
 window.Cookies = require("js-cookie");
+const BOOTSTRAP_SM_BREAKPOINT = 575;
 
 // custom JS modules
 import {
@@ -116,7 +117,9 @@ const ATD_DocklessMap = (function() {
     };
     docklessMap.Draw = new MapboxDraw(drawOptions);
     docklessMap.map.addControl(docklessMap.Draw, "top-left");
-    $("#map-container").css("height", window.innerHeight);
+    if (window.innerWidth < BOOTSTRAP_SM_BREAKPOINT) {
+      $("#map-container").css("height", window.innerHeight);
+    }
   };
 
   // TODO: Break this down into smaller pieces
@@ -281,7 +284,10 @@ const ATD_DocklessMap = (function() {
 
     $(window).smartresize(() => {
       docklessMap.map.resize();
-      $("#map-container").css("height", window.innerHeight);
+
+      if (window.innerWidth < BOOTSTRAP_SM_BREAKPOINT) {
+        $("#map-container").css("height", window.innerHeight);
+      }
     });
   };
 
